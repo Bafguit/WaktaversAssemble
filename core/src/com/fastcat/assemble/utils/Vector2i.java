40743634cb0bea7b65a5368162573338d4bbcea7
Kuiprux <kuiprux@gmail.com>
2023-04-01 01:47:08 +0900

    Fixed a bug in pathfinding

--- core/src/com/fastcat/assemble/utils/Vector2i.java
+++ core/src/com/fastcat/assemble/utils/Vector2i.java
@@ -2,6 +2,8 @@ package com.fastcat.assemble.utils;
 
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.Objects;
+
 public class Vector2i {
     public int x;
     public int y;
@@ -33,5 +35,23 @@ public class Vector2i {
 
     public static Vector2i getVector2i(Vector2 v2) {
         return new Vector2i((int) v2.x, (int) v2.y);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Vector2i) {
+            return ((Vector2i) obj).x == x && ((Vector2i) obj).y == y;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "{x: " + x + ", y: " + y + "}";
     }
 }
