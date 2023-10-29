1e0153374609376fc96a7862d26b10990721036b
FastCat <bafguit9929@gmail.com>
2023-10-29 15:58:58 +0900

    스프라이트 애니메이션 클래스 추가 (수정 필요)

---
+++ core/src/com/fastcat/assemble/utils/SpriteAnimation.java
@@ -0,0 +1,99 @@
+package com.fastcat.assemble.utils;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Queue;
+import com.fastcat.assemble.MousseAdventure;
+
+import java.util.HashMap;
+
+public class SpriteAnimation {
+
+    private final HashMap<String, SpriteAnimationData> animations = new HashMap<>();
+
+    private SpriteAnimationData current;
+    private Queue<SpriteAnimationData> next = new Queue<>();
+    private Color color = Color.WHITE.cpy();
+    private float timer = 0f;
+    private boolean isRunning = true;
+
+    public Vector2i pos, size;
+
+    public SpriteAnimation() {
+
+    }
+
+    public void setAnimation(String key) {
+        isRunning = true;
+        timer = 0f;
+        current = animations.get(key);
+    }
+
+    public void addAnimation(String key) {
+        next.addLast(animations.get(key));
+    }
+
+    public void render(SpriteBatch sb) {
+        boolean hasAnimation = true;
+
+        if(current != null) {
+            if(!current.isLoop && timer > current.duration) {
+                if(next.size > 0) {
+                    timer = 0f;
+                    current = next.removeFirst();
+                } else {
+                    timer = current.duration - current.frameDuration;
+                    isRunning = false;
+                }
+            }
+        } else {
+            if(next.size > 0) {
+                timer = 0f;
+                current = next.removeFirst();
+            } else {
+                hasAnimation = false;
+            }
+        }
+
+        if(hasAnimation) {
+            Sprite frame = current.getFrame(timer);
+            sb.draw(frame, pos.x, pos.y);
+            if(isRunning) tickDuration();
+        }
+    }
+
+    private void tickDuration() {
+        timer += MousseAdventure.tick;
+        if(timer > current.duration) {
+            timer -= current.duration;
+        }
+    }
+
+    public class SpriteAnimationData {
+
+        private final Sprite[] frames;
+        private float duration, frameDuration, timescale = 1.0f;
+        private boolean isLoop;
+        private Vector2i axis;
+
+        public final String key;
+
+        //todo json 불러오는걸로 교체 필요
+        public SpriteAnimationData(String key, Array<Sprite> sprites, float duration) {
+            this.key = key;
+            frames = sprites.items;
+            frameDuration = duration;
+            this.duration = frameDuration * frames.length;
+        }
+
+        public Sprite getFrame(float time) {
+            return getFrame((int) (time / frameDuration));
+        }
+
+        public Sprite getFrame(int number) {
+            return frames[number % frames.length];
+        }
+    }
+}
