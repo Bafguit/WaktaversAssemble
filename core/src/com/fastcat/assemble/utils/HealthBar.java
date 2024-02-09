6202a3b53367c2b7c452e44aea4df9a81a347e74
FastCat <bafguit9929@gmail.com>
2024-02-09 14:11:42 +0000

    hbBar 완성

--- core/src/com/fastcat/assemble/utils/HealthBar.java
+++ core/src/com/fastcat/assemble/utils/HealthBar.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Widget;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar.ProgressBarStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
@@ -23,47 +24,34 @@ import com.fastcat.assemble.handlers.InputHandler;
 import com.fastcat.assemble.handlers.FontHandler.FontData;
 import com.fastcat.assemble.interfaces.OnHealthUpdated;
 
-public class HealthBar extends Table implements OnHealthUpdated {
+public class HealthBar extends Widget implements OnHealthUpdated {
 
     private static final String BLUE = FontHandler.getColorKey("b");
     private static final String GREEN = FontHandler.getColorKey("g");
 
-    private ProgressBar hb, yet;
+    private TextureRegionDrawable bg, hb, yet, block, barrier;
     private Label text;
+    private float yetHealth, health, timer = 0, wScale = 1;
 
     public AbstractEntity entity;
 
     public HealthBar(AbstractEntity entity) {
-        this(entity, 280);
+        this(entity, 148);
     }
 
     public HealthBar(AbstractEntity entity, final float width) {
         this.entity = entity;
-        Button bg = new Button(new TextureRegionDrawable(FileHandler.getPng("ui/hb")));
-        hb = new ProgressBar(0, this.entity.maxHealth, 1, false, new ProgressBarStyle(new TextureRegionDrawable(FileHandler.getPng("ui/hb")), null));
-        hb.setWidth(width);
-        hb.setTouchable(Touchable.disabled);
-        yet = new ProgressBar(0, this.entity.maxHealth, 1, false, new ProgressBarStyle(new TextureRegionDrawable(FileHandler.getPng("ui/hb_yet")), null));
-        yet.setWidth(width);
-        yet.setTouchable(Touchable.disabled);
-        yet.setAnimateDuration(1);
-        Button block = new Button(new TextureRegionDrawable(FileHandler.getPng("ui/hb_block"))) {
-            AbstractEntity ent = entity;
-            
-            public void act(float delta) {
-                if(ent.block > 0) setColor(1,1,1,1);
-                else setColor(1,1,1,0);
-            }
-        };
-        Button barrier = new Button(new TextureRegionDrawable(FileHandler.getPng("ui/hb_barrier"))) {
-            AbstractEntity ent = entity;
-            
-            public void act(float delta) {
-                if(ent.barrier > 0) setColor(1,1,1,1);
-                else setColor(1,1,1,0);
-            }
-        };
-        bg.setColor(0.5f, 0.5f, 0.5f, 1);
+        bg = new TextureRegionDrawable(FileHandler.getPng("ui/hb"));
+        hb = new TextureRegionDrawable(FileHandler.getPng("ui/hb"));
+        yet = new TextureRegionDrawable(FileHandler.getPng("ui/hb_yet"));
+        block = new TextureRegionDrawable(FileHandler.getPng("ui/hb_block"));
+        barrier = new TextureRegionDrawable(FileHandler.getPng("ui/hb_barrier"));
+
+        health = (float) entity.health / (float) entity.maxHealth;
+        setWidth(width);
+        setHeight(12);
+        wScale = (float) width / 148;
+
         entity.healthBar = this;
 
         String t = entity.health + "/" + entity.maxHealth;
@@ -72,28 +60,47 @@ public class HealthBar extends Table implements OnHealthUpdated {
 
         text = new Label(t, new LabelStyle(FontHandler.BF_HEALTH, Color.WHITE));
         text.setAlignment(Align.center);
-
-        this.add(bg).width(width);
-        this.row();
-        this.add(yet).width(width).padTop(-8);
-        this.row();
-        this.add(hb).width(width).padTop(-8);
-        this.row();
-        this.add(block).width(width + 2).padTop(-9).padLeft(-1);
-        this.row();
-        this.add(barrier).width(width).padTop(-9);
-        this.row();
-        this.add(text).height(16).padTop(-13).width(width).center();
+        text.setWidth(width);
     }
 
     @Override
-    public void onHealthUpdated(int amount) {
-        hb.setValue(entity.health);
-        //hb.updateVisualValue();
-        yet.setValue(entity.health);
+    public void act(float delta) {
+        if(timer == 0) {
+            if(yetHealth > health) yetHealth -= delta;
+            if(yetHealth < health) yetHealth = health;
+        } else {
+            timer -= delta;
+            if(timer < 0) timer = 0;
+        }
+        text.act(delta);
+    }
+    
+    @Override
+	public void draw (Batch batch, float parentAlpha) {
+        float x = getX(), y = getY(), width = getWidth(), height = getHeight();
+        Color c = batch.getColor().cpy();
+        batch.setColor(c.r * 0.5f, c.g * 0.5f, c.b * 0.5f, c.a);
+        bg.draw(batch, x, y, 0, 0, width, height, 1, 1, 0);
+        batch.setColor(c);
+        yet.draw(batch, x, y, 0, 0, width, height, yetHealth, 1, 0);
+        hb.draw(batch, x, y, 0, 0, width, height, health, 1, 0);
+        if(entity.block <= 0) batch.setColor(c.r, c.g, c.b, 0);
+        block.draw(batch, x - (1.5f * wScale), y - (1.5f * wScale), 0, 0, width, height, 1, 1, 0);
+        if(entity.barrier <= 0) batch.setColor(c.r, c.g, c.b, 0);
+        else batch.setColor(c.r, c.g, c.b, c.a);
+        barrier.draw(batch, x, y, 0, 0, width, height, 1, 1, 0);
+        batch.setColor(c);
         String t = entity.health + "/" + entity.maxHealth;
         if(entity.barrier > 0) t += GREEN + " +" + entity.barrier;
         if(entity.block > 0) t += BLUE + " +" + entity.block;
         text.setText(t);
+        text.setPosition(x, y - 2);
+        text.draw(batch, parentAlpha);
+    }
+
+    @Override
+    public void onHealthUpdated(int amount) {
+        health = (float) entity.health / (float) entity.maxHealth;
+        timer = 1;
     }
 }
